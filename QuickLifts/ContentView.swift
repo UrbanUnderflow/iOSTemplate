968168b39efc6fbd641013f40578678cf1359a23
pitchblackio <founders@pblack.io>
2023-06-24 20:37:53 -0400

    Initial Commit

---
+++ QuickLifts/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  QuickLifts
+//
+//  Created by Tremaine Grant on 6/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuickLifts/QuickLiftsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickLiftsApp.swift
+//  QuickLifts
+//
+//  Created by Tremaine Grant on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct QuickLiftsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
